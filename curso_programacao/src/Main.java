fb483f2e2230eb22d1de86518edc45c37c2a0047
Antonio Rodrigues dos Santos <antoniors1967@gmail.com>
2024-06-22 20:18:18 -0300

    Projeto criado

---
+++ curso_programacao/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	double raio = 150.00;
+	double PI = 3.14159;
+	double area = PI*Math.pow(raio, 2);
+		System.out.println("A = "+ area);
+	}
+
+}
